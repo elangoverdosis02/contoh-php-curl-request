3fe0f81ba658825c27bcfb86dd51ca64ef8092a7
deny siswanto <154708871+elangoverdosis02@users.noreply.github.com>
2025-07-01 18:06:04 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,16 @@
 {
-  "CURLE_OK"
+    "status": true,
+    "data": {
+        "token": "7a15b33accfeb973b7790f9c7a9220e4",
+        "NAME_MOD": "XYOURZONE",
+        "EXP_MOD": "2025-07-02 13:43:19",
+        "device": "1",
+        "MOD_STATUS": "Safe",
+        "iniVip": "20",
+        "DURASI": "0 hari 19 jam",
+        "rng": 1751367842,
+        "client": "FREE-NMNXUOGZ",
+        "AutoSkills": "on",
+        "serial": "cef77717-d907-3d2d-bd2f-e4a2e8aa8b62"
+    }
 }
